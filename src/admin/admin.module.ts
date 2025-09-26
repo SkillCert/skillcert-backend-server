c56f8549dbd22a4625eac69a5d2156d938d55e0d
Jenny <Jennygranadosquiros@gmail.com>
2025-09-26 12:54:32 -0600

    edit: imports and exports

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../users/entities/user.entity';
 
 @Module({
   controllers: [AdminController],
+  imports: [TypeOrmModule.forFeature([User])],
+  exports: [TypeOrmModule],
 })
 export class AdminModule {}
